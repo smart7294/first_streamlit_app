4c0b0b11a6457f075f68697fb0b38fdf0351287d
Sirine Martins <101058656+smart7294@users.noreply.github.com>
2022-12-24 21:28:23 +0000

    Update streamlit_app.py
    Correctly capitalized fruit choice.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,10 +27,10 @@ streamlit.dataframe(fruits_to_show)
 #Display API response
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+streamlit.write('The user entered', fruit_choice.capitalize())
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice.capitalize())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #Normalize the API response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
