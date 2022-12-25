98fd879a1399e2a6eaaf3860a3dda5309f559653
Sirine Martins <101058656+smart7294@users.noreply.github.com>
2022-12-25 19:24:38 +0000

    Update streamlit_app.py
    Moved the Fruityvice section of code into a Try-Except.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,20 +29,19 @@ streamlit.dataframe(fruits_to_show)
 
 #Display API response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice.capitalize())
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+     #streamlit.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #Normalize the API response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #Output normalized response as a table
+    streamlit.dataframe(fruityvice_normalized)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#Normalize the API response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#Output normalized response as a table
-streamlit.dataframe(fruityvice_normalized)
-
-
-#Stop program at this point to troubleshoot
-streamlit.stop()
+except URLError as e:
+  streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
